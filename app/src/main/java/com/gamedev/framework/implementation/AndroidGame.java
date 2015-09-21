92d8b78a668c8a38fc6f0e736939dcb4451a22e0
Sergey Papushin <spapushin@gmail.com>
2015-09-21 11:03:28 -0500

    Remove unused imports

--- app/src/main/java/com/gamedev/framework/implementation/AndroidGame.java
+++ app/src/main/java/com/gamedev/framework/implementation/AndroidGame.java
@@ -1,13 +1,10 @@
 package com.gamedev.framework.implementation;
 
 import android.app.Activity;
-import android.content.Context;
 import android.content.res.Configuration;
 import android.graphics.Bitmap;
 import android.graphics.Bitmap.Config;
 import android.os.Bundle;
-import android.os.PowerManager;
-import android.os.PowerManager.WakeLock;
 import android.view.Window;
 import android.view.WindowManager;
 
